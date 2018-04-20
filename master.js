78624ef136927920df8ffde8dcf1661d60c92978
PiethonCoder <austinthemichaud@gmail.com>
2018-04-20 12:48:46 -0400

    Add files via upload

--- master.js
+++ master.js
@@ -1,19 +1,42 @@
+
+var reader = new FileReader();
+ 
 var wrapper = new Vue({
   el:"#container",
   data:{
-      html:"",
-      css:"",
-      code:"<p>Hello World</p>"
+	  htmlCode:"",
+	  cssCode:"",
+      htmlLink:"",
+      cssLink:""
   },
   methods:{
-    convert: function(css,html){
-
+	  
+	loadHTML:function(){
+		var _this = this;
+	    reader.onload = function(e) {
+			_this.htmlCode = reader.result;
+			console.log(reader.result)
+		} 
+		
+		reader.readAsText(document.getElementById("html").files[0]);		
+	},
+	loadCSS:function(){
+		var _this = this;
+	    reader.onload = function(e) {
+			_this.cssCode = reader.result;
+			console.log(reader.result)
+		} 
+		
+		reader.readAsText(document.getElementById("css").files[0]);
     }
   },
-
   computed:{
-
+	code: function(){
+		return "<p>Hello World</p>"
+		// return this.cssCode + this.htmlCode; 
+	}
   }
 
-
 })
+
+// || "<p>Hello World</p>";
