560c4427e7241be9eb0a6887aaae060d8347865d
PiethonCoder <austinthemichaud@gmail.com>
2018-11-22 23:13:31 -0500

    Add files via upload

--- plugins/encode.js
+++ plugins/encode.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var quot = "'"
 
 
@@ -49,55 +48,3 @@ function Hex(dec) {
 
 
 
-=======
-var quot = "'"
-
-
-function Encrypt(code,fullenc) {
-    if (fullenc) {
-        return EncryptAll(code)
-    } else {
-        return EncryptBasic(code)
-    }
-}
-
-function EncryptBasic(code) {
-    var NewCode = escape(code)
-    NewCode = '<' + 'script language=javascript>document.write(unescape(' + quot +
-        NewCode + quot + '))<' + '/script>\n'
-    return NewCode
-}
-
-function EncryptAll(code) {
-    NewCode = ""
-    var OldCode = code
-    for (var i = 0; i < OldCode.length; i++) {
-        NewCode = NewCode + Hex(OldCode.charCodeAt(i))
-    }
-    NewCode = '<' + 'script language=javascript>document.write(unescape(' + quot +
-        NewCode + quot + '))<' + '/script>\n'
-    return NewCode
-    //--adds linebreak after encoded block.
-    // the added linebreaks prevent preview in interim decoding, 
-    //  but will view in final decoding 
-}
-
-function Decrypt(code) {
-    var NewCode = unescape(code)
-    NewCode = NewCode.replace("<script language=javascript>document.write(unescape('", "")
-    NewCode = NewCode.replace("'))<" + "/script>", "")
-    return NewCode
-}
-
-function Hex(dec) {
-    var hexbase = "0123456789ABCDEF"
-    hx_hi = dec / 16;
-    hx_lo = dec % 16;
-    hx = hexbase.substr(hx_hi, 1) + hexbase.substr(hx_lo, 1)
-    hexval = '%' + hx
-    return hexval;
-}
-
-
-
->>>>>>> e59cc6e78286fbd45621df85522de01c941af342
